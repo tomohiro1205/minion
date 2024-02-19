4a32fea0b130706d7392eff0d7773dd11ac77322
tomohiro1205 <tomohiro_1205@yahoo.co.jp>
2024-02-19 09:26:48 +0900

    Part11の可読性もやった

--- minion/ContentView.swift
+++ minion/ContentView.swift
@@ -10,75 +10,70 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            ZStack{
-                Rectangle()
-                    .foregroundColor(.yellow)
-                    .frame(width: 300, height: 300)
-                VStack{
-                    HStack{
-                        Rectangle()
-                            .foregroundColor(.black)
-                            .frame(width: 60, height: 20)
-                        ZStack{
-                            Rectangle()
-                                .foregroundColor(.gray)
-                                .frame(width: 90, height: 90)
-                            Rectangle()
-                                .foregroundColor(.white)
-                                .frame(width: 70, height: 70)
-                            Rectangle()
-                                .foregroundColor(.black)
-                                .frame(width: 20, height: 20)
-
-                        } //ZStack ここまで
-                        ZStack{
-                            Rectangle()
-                                .foregroundColor(.gray)
-                                .frame(width: 90, height: 90)
-                            Rectangle()
-                                .foregroundColor(.white)
-                                .frame(width: 70, height: 70)
-                            Rectangle()
-                                .foregroundColor(.black)
-                                .frame(width: 20, height: 20)
-                        } //ZStackここまで
-                        Rectangle()
-                            .foregroundColor(.black)
-                            .frame(width: 60, height: 20)
-                    } //HStack ここまで
-                    ZStack {
-
-                        Rectangle()
-                            .foregroundColor(.black)
-                            .frame(width: 70, height: 40)
-                        VStack{
-                            Rectangle()
-                                .foregroundColor(.white)
-                                .frame(width: 60, height: 10)
-                            Rectangle()
-                                .foregroundColor(.red)
-                                .frame(width: 60, height: 10)
-                        } //Vstackここまで
-                    } //Zstackここまで
-                } //VStack ここまで
-            } // ZStack ここまで
-
-            Rectangle()
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                .frame(width: 300, height: 100)
-            HStack{
-                Rectangle()
-                    .foregroundColor(.black)
-                    .frame(width:70, height:30)
-                Rectangle()
-                    .foregroundColor(.black)
-                    .frame(width:70, height:30)
-            } // HStackここまで
+            FaceView()
+            PantsView()
+            FootView()
         } //VStackここまで
     }//body ここまで
 }
 
+struct FaceView: View{
+    var body: some View{
+        ZStack{
+            RectangleView(color: .yellow, width: 300, height: 300)
+            VStack{
+                HStack{
+                    RectangleView(color: .black, width: 60, height: 20)
+                    ZStack{
+                        RectangleView(color: .gray, width: 90, height: 90)
+                        RectangleView(color: .white, width: 70, height: 70)
+                        RectangleView(color: .black, width: 20, height: 20)
+                    } //ZStack ここまで
+                    ZStack{
+                        RectangleView(color: .gray, width: 90, height: 90)
+                        RectangleView(color: .white, width: 70, height: 70)
+                        RectangleView(color: .black, width: 20, height: 20)
+                    } //ZStackここまで
+                    RectangleView(color: .black, width: 60, height: 20)
+                } //HStack ここまで
+                ZStack {
+                    RectangleView(color: .black, width: 70, height: 40)
+                    VStack{
+                        RectangleView(color: .white, width: 60, height: 10)
+                        RectangleView(color: .red, width: 60, height: 10)
+                    } //VStackここまで
+                } //ZStackここまで
+            } //VStack ここまで
+        }
+    }
+}
 
+struct PantsView: View{
+    var body: some View{
+        RectangleView(color: .blue, width: 300, height: 100)
+    }
+}
+
+struct FootView: View{
+    var body: some View{
+        HStack{
+            RectangleView(color: .black, width: 70, height: 30)
+            RectangleView(color: .black, width: 70, height: 30)
+        } // HStackここまで
+    }
+}
+
+struct RectangleView: View{
+    let color: Color
+    let width: CGFloat
+    let height: CGFloat
+
+    var body: some View{
+        Rectangle()
+            .foregroundColor(color)
+            .frame(width:width, height:height)
+    }
+}
 #Preview {
     ContentView()
 }
